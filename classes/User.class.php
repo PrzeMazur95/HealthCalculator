5fa84297a7c700dc4ca60a976bd4d23284d5a2a5
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-03 10:52:01 +0200

    Add user model  class, and methods to register in it

---
+++ classes/User.class.php
@@ -0,0 +1,53 @@
+<?php
+
+class User extends Dbh {
+
+    protected function setUser($username, $pwd, $email){
+
+        $stmt = $this->connect()->prepare('INSERT INTO Users (username, pwd, email) VALUES (?, ?, ?)');
+
+        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+        if(!$stmt->execute(array($username, $hashedPwd, $email))){
+
+            $stmt = null;
+            header("location: ../index.html?error=stmtfailed1");
+            exit();
+
+        }
+
+        $stmt = null;
+
+    }
+
+    protected function checkUser($username, $email){
+
+        $stmt = $this->connect()->prepare('SELECT username FROM Users WHERE username = ? OR email = ?');
+
+        if(!$stmt->execute(array($username, $email))){
+
+            $stmt = null;
+            header("location: ../index.html?error=stmtfailed2");
+            exit();
+
+        }
+
+        $resultCheck;
+
+        if($stmt->rowCount() > 0){
+
+            $resultCheck = false;
+
+        } else {
+
+            $resultCheck = true;
+
+        }
+
+        return $resultCheck;
+
+    }
+
+}
+
+?>
