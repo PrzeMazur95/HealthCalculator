01a205269623b379ece6a019ac88a0955e7692d2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-03 10:50:53 +0200

    Add signup include file

---
+++ includes/signup.inc.php
@@ -0,0 +1,20 @@
+<?php
+include_once "autoloader.inc.php";
+
+    if(isset($_POST['submit'])){
+
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $pwd = $_POST['password'];
+        $pwdRepeat = $_POST['passwordconfirm'];
+
+
+        $signup = new UserController($username, $pwd, $pwdRepeat, $email);
+
+        $signup->signupUser();
+
+        header("location: ../login.php?error=created");
+
+    }
+
+?>
