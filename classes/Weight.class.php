422491aa0e5cf4e046551ebd97018c1f8e97d736
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-04 09:55:08 +0200

    Add set weight method id weight Model class

--- classes/Weight.class.php
+++ classes/Weight.class.php
@@ -2,5 +2,22 @@
 
 class Weight extends Dbh {
 
+    protected function setWeight($user_id, $weight, $date, $comment){
+
+        $stmt = $this->connect()->prepare('INSERT INTO Weight (user_id, weight, datte, comment) VALUES (?, ?, ?, ?)');
+
+        if(!$stmt->execute(array($user_id, $weight, $date, $comment))){
+
+            $stmt = null;
+            header("location: ../weight.php?error=stmtfailed");
+            exit();
+
+        }
+
+        $stmt = null;
+        header("location: ../weight.php?info=properlyadded");
+
+    }
+
 }
 ?>
