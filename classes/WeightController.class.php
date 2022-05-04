742c2e12865acf32e1ec2ab08368045fe7515274
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-04 09:54:41 +0200

    Add addWeight method in Weight Controller

--- classes/WeightController.class.php
+++ classes/WeightController.class.php
@@ -2,7 +2,55 @@
 
 class WeightController extends Weight {
 
-    
+    private $user_id;
+    private $weight;
+    private $date;
+    private $comment;
+
+    public function __construct($user_id, $weight, $comment){
+
+
+        $this->user_id = $user_id;
+        $this->weight = $weight;
+        $this->comment = $comment;
+        $this->date = date("Y/m/d");
+
+    }
+
+
+    public function addWeight(){
+
+        if($this->emptyInput() == false){
+
+            header("location: ../weight.php?error=emptyinput");
+            exit();
+
+        }
+
+        $this->setWeight($this->user_id, $this->weight, $this->date, $this->comment);
+
+    }
+
+    private function emptyInput(){
+
+        $result;
+
+        if(empty($this->user_id || $this->weight || $this->date)){
+
+            $result = false;
+
+        } else {
+
+            $result = true;
+
+        }
+
+        return $result;
+
+    }
+
 }
+    
+
 
 ?>
