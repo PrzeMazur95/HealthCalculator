860fe14846a0d41851f8df0e1b80b9e1d708e715
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-04 11:37:22 +0200

    Add showWeight method in WeightView class

--- classes/WeightView.class.php
+++ classes/WeightView.class.php
@@ -1,6 +1,14 @@
 <?php
 
 class WeightView extends Weight {
+    
+    public function showWeight($user_id){
+
+        $results = $this->getWeight($user_id);
+
+        return $results;
+
+    }
 
 }
 ?>
