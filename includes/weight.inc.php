5428f4d7e8f4538d1b2a5534a340e37d337f4932
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-04 09:54:01 +0200

    Add on result submit handler

--- includes/weight.inc.php
+++ includes/weight.inc.php
@@ -1,6 +1,39 @@
 <?php
 include_once "autoloader.inc.php";
+session_start();
 
- 
+    if(isset($_POST['submit'])){
+
+        if(empty($_POST['weight'])){
+
+            header("location: ../weight.php?error=emptyweight");
+
+        } else {
+
+            if(strpos($_POST['weight'], '.')){
+                
+                $weight = str_replace(".", ",", $_POST['weight']);
+
+            } else {
+
+                $weight = $_POST['weight'];
+            }
+
+            $user_id = $_SESSION['userid'];
+            $comment = $_POST['comment'];
+
+            $weight = new WeightController($user_id, $weight, $comment);
+
+            $weight->addWeight();
+
+            header("location: ../weight.php?info=properlyAdded");
+
+        }
+
+    } else {
+
+        header("location: ../weight.php?error=submit");
+
+    }
 
 ?>
