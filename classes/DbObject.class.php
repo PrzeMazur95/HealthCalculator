3be2d12d28081e9b4b2f77804e2ae7e7c108e241
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-06 11:28:46 +0200

    Add find object by id method in Dbobject Model class

--- classes/DbObject.class.php
+++ classes/DbObject.class.php
@@ -26,5 +26,32 @@ class DbObject extends Dbh {
     }  
 
 
+    protected function Find_by_id_and_table($table, $id, $userid){
+
+        $stmt = $this->connect()->prepare("SELECT * FROM ".$table." WHERE id = (?) AND user_id = (?) LIMIT 1");
+
+        $stmt->execute([$id, $userid]);
+
+        $results = $stmt->fetchAll();
+
+        if(!$results){
+
+            return false;
+            $stmt = null;
+            exit();
+
+        } else {
+
+            return $results;
+            $stmt = null;
+            exit();
+
+        }
+
+        return $results;
+
+    }  
+        
+
 }
 ?>
