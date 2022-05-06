1d1853d3f3fee6309aa0763d8a8806679aa19eba
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-06 11:32:56 +0200

    Add findy by id method in DbObjectView class

--- classes/DbObjectView.class.php
+++ classes/DbObjectView.class.php
@@ -2,6 +2,20 @@
 
 class DbObjectView extends DbObject {
 
+    public function Find_by_id($table, $id, $userid){
+
+        if(!is_numeric($id) || is_string($table || !is_numeric($userid))){
+
+            header("location: ../index.php");
+
+        } else {
+
+            $object = $this->Find_by_id_and_table($table, $id, $userid);
+            return $object;
+
+        }
+
+    }
 
 }
     
