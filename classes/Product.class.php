576468c176b87d0d2685dce8987e89580958430a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-11 10:27:33 +0200

    Add new methow to save also files to product model class

--- classes/Product.class.php
+++ classes/Product.class.php
@@ -28,5 +28,33 @@ class Product extends Dbh {
  
     }
 
+    protected function setProductAndPhoto($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename){
+
+
+        $sql = "INSERT INTO Products (name,  kcal,  protein,  animal_protein, vegetable_protein,  fat,  saturated_fat,  monounsaturated_fat, polyunsaturated_fat, omega3_acid, omega6_acid, carbohydrates, net_carbohydrates, sugar,  fiber,  salt, cholesterol,  witamin_k, witamin_a, witamin_b1,  witamin_b2,  witamin_b5, witamin_b6,  biotin,  folic_acid,  witamin_b12,  witamin_c,  witamin_d,  witamin_e, witamin_pp, calcium, chlorine, magnesium, phosphorus, potassium, sodium, iron, zinc, copper,  manganese, molybdenum, iodine, fluorine, chrome, selenium, description, filename) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $stmt = $this->connect()->prepare($sql);
+        if(!$stmt->execute(array($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename))){
+            // print_r($stmt->errorInfo());
+            // die();
+            $stmt=null;
+            return false;
+            exit(); 
+
+            header("location: ../product.php?error=stmtfailed");
+            
+            
+        } else {
+
+            $stmt=null;
+            return true;
+            exit();
+
+        }
+
+ 
+    }
+
+    
+
 }
 ?>
