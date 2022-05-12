d755cdefbe1bb68410b4d9108d5b5c5b81d68d6f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-12 18:16:20 +0200

    Add view button, and proper redirect

--- products.php
+++ products.php
@@ -102,8 +102,9 @@
                                             <td class="text-center"><?php echo $Result['name'] ?></td>
                                             <td class="text-center"><?php echo $Result['kcal'] ?></td>
                                             <td class="text-center">
-                                            <a href="includes/edit.php?id=<?php echo $row['id']; ?>&table=Weight&action=edit&userid=<?php echo $_SESSION['userid'] ?>" class="btn btn-primary btn-sm">Edit</a>
-                                            <a href="includes/delete.php?id=<?php echo $row['id']; ?>&table=Weight&userid=<?php echo $_SESSION['userid'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure, you want to delete this result?')">Delete</a>
+                                            <a href="includes/edit.php?id=<?php echo $row['id']; ?>&table=Products&action=edit" class="btn btn-primary btn-sm">Edit</a>
+                                            <a href="includes/delete.php?id=<?php echo $row['id']; ?>&table=Products" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure, you want to delete this result?')">Delete</a>
+                                            <a href="includes/view_product.php?id=<?php echo $row['id']; ?>" class="btn btn-info btn-sm">View</a>
                                             </td>      
                                         </tr>
 
