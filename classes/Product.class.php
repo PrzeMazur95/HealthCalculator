de6bcbac989afe8ceb55d2a2e16e7182a28a60be
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-13 08:04:30 +0200

    Add additional property to setProductAndPhoto ethod in Product model class

--- classes/Product.class.php
+++ classes/Product.class.php
@@ -28,19 +28,19 @@ class Product extends Dbh {
  
     }
 
-    protected function setProductAndPhoto($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename){
+    protected function setProductAndPhoto($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename, $userid){
 
 
-        $sql = "INSERT INTO Products (name,  kcal,  protein,  animal_protein, vegetable_protein,  fat,  saturated_fat,  monounsaturated_fat, polyunsaturated_fat, omega3_acid, omega6_acid, carbohydrates, net_carbohydrates, sugar,  fiber,  salt, cholesterol,  witamin_k, witamin_a, witamin_b1,  witamin_b2,  witamin_b5, witamin_b6,  biotin,  folic_acid,  witamin_b12,  witamin_c,  witamin_d,  witamin_e, witamin_pp, calcium, chlorine, magnesium, phosphorus, potassium, sodium, iron, zinc, copper,  manganese, molybdenum, iodine, fluorine, chrome, selenium, description, filename) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $sql = "INSERT INTO Products (name,  kcal,  protein,  animal_protein, vegetable_protein,  fat,  saturated_fat,  monounsaturated_fat, polyunsaturated_fat, omega3_acid, omega6_acid, carbohydrates, net_carbohydrates, sugar,  fiber,  salt, cholesterol,  witamin_k, witamin_a, witamin_b1,  witamin_b2,  witamin_b5, witamin_b6,  biotin,  folic_acid,  witamin_b12,  witamin_c,  witamin_d,  witamin_e, witamin_pp, calcium, chlorine, magnesium, phosphorus, potassium, sodium, iron, zinc, copper,  manganese, molybdenum, iodine, fluorine, chrome, selenium, description, filename, userid) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         $stmt = $this->connect()->prepare($sql);
-        if(!$stmt->execute(array($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename))){
+        if(!$stmt->execute(array($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $filename, $userid))){
             // print_r($stmt->errorInfo());
             // die();
             $stmt=null;
             return false;
             exit(); 
 
-            header("location: ../product.php?error=stmtfailed");
+            header("location: ../add_product.php?error=stmtfailed");
             
             
         } else {
