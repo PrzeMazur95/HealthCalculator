16932235a6033dd5c977edfcc9d625bb3962230c
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-17 20:54:58 +0200

    Instantiate ProductController, grabbing user id

--- includes/edit_product.inc.php
+++ includes/edit_product.inc.php
@@ -5,7 +5,7 @@ session_start();
 
 $properties=array();
 
-$mainInformations = array('Name'=>'name', 'kCal'=>'kcal', 'Protein'=>'protein', 'Carbs'=>'carbohydrates', 'Fat'=>'fat');
+$mainInformations = array('Name'=>'name', 'kCal'=>'kcal', 'Protein'=>'protein', 'Carbs'=>'carbohydrates', 'Fat'=>'fat', 'Sugar'=>'sugar');
 
 $additionalFields = array('Ani. Protein'=>'animal_protein', 'Vege. Protein'=>'vegetable_protein', 'Sat. Fat'=>'saturated_fat', 
 'Mono_Fat'=>'monounsaturated_fat', 'Poly Fat'=>'polyunsaturated_fat', 'Omega3'=>'omega3_acid', 'Omega6'=>'omega6_acid', 
@@ -16,7 +16,7 @@ $additionalFields = array('Ani. Protein'=>'animal_protein', 'Vege. Protein'=>'ve
 'Witamin PP'=>'witamin_pp', 'Calcium'=>'calcium', 'Chlorine'=>'chlorine', 'Magnesium'=>'magnesium', 
 'Phosphorus'=>'phosphorus', 'Potassium'=>'potassium', 'Sodium'=>'sodium', 'Iron'=>'iron', 
 'Zinc'=>'zinc', 'Copper'=>'copper', 'Manganese'=>'manganese', 'Molybdenum'=>'molybdenum', 
-'Iodine'=>'iodine', 'Fluorine'=>'fluorine', 'Chrome'=>'chrome', 'Selenium'=>'selenium');
+'Iodine'=>'iodine', 'Fluorine'=>'fluorine', 'Chrome'=>'chrome', 'Selenium'=>'selenium', 'Description'=>'description');
 
 if(isset($_POST['update'])){
 
@@ -50,67 +50,17 @@ if(isset($_POST['update'])){
     }
 
     $properties['filename']=$_FILES['photo']['name'];
+    $properties['userid']=$_SESSION['userid'];
 
-    print_r($properties);
-    die();  
+    // print_r($properties);
+    // die();  
    
 
 }
 
-
-if(isset($_POST['submit'])){
-
-    $name = $_POST["name"];
-    $kcal = $_POST["kcal"]; 
-    $protein = $_POST["protein"];
-    $animal_protein = $_POST["animal_protein"];
-    $vegetable_protein = $_POST["vegetable_protein"];  
-    $fat = $_POST["fat"];
-    $saturated_fat = $_POST["saturated_fat"];
-    $monounsaturated_fat = $_POST["monounsaturated_fat"];
-    $polyunsaturated_fat = $_POST["polyunsaturated_fat"];
-    $omega3_acid = $_POST["omega3_acid"]; 
-    $omega6_acid = $_POST["omega6_acid"]; 
-    $carbohydrates = $_POST["carbohydrates"];
-    $net_carbohydrates = $_POST["net_carbohydrates"];
-    $sugar = $_POST["sugar"];  
-    $fiber = $_POST["fiber"]; 
-    $salt = $_POST["salt"]; 
-    $cholesterol = $_POST["cholesterol"]; 
-    $witamin_k = $_POST["witamin_k"];
-    $witamin_a = $_POST["witamin_a"];
-    $witamin_b1 = $_POST["witamin_b1"];  
-    $witamin_b2 = $_POST["witamin_b2"];  
-    $witamin_b5 = $_POST["witamin_b5"];
-    $witamin_b6 = $_POST["witamin_b6"]; 
-    $biotin = $_POST["biotin"];
-    $folic_acid = $_POST["folic_acid"];  
-    $witamin_b12 = $_POST["witamin_b12"];  
-    $witamin_c = $_POST["witamin_c"];  
-    $witamin_d = $_POST["witamin_d"];  
-    $witamin_e = $_POST["witamin_e"]; 
-    $witamin_pp = $_POST["witamin_pp"];
-    $calcium = $_POST["calcium"];
-    $chlorine = $_POST["chlorine"];
-    $magnesium = $_POST["magnesium"];
-    $phosphorus = $_POST["phosphorus"];
-    $potassium = $_POST["potassium"]; 
-    $sodium = $_POST["sodium"];
-    $iron = $_POST["iron"];
-    $zinc = $_POST["zinc"];
-    $copper = $_POST["copper"];
-    $manganese = $_POST["manganese"];
-    $molybdenum = $_POST["molybdenum"];
-    $iodine = $_POST["iodine"];
-    $fluorine = $_POST["fluorine"];
-    $chrome = $_POST["chrome"]; 
-    $selenium = $_POST["selenium"];
-    $description = $_POST["description"];
-    $photo = $_FILES['photo'];
-    $_SESSION['error'] = "";
-    $userid = $_SESSION['userid'];
  
-    $file = new ProductController($name,  $kcal,  $protein,  $animal_protein, $vegetable_protein,  $fat,  $saturated_fat,  $monounsaturated_fat, $polyunsaturated_fat, $omega3_acid, $omega6_acid, $carbohydrates, $net_carbohydrates, $sugar,  $fiber,  $salt, $cholesterol, $witamin_k, $witamin_a, $witamin_b1,  $witamin_b2,  $witamin_b5, $witamin_b6,  $biotin,  $folic_acid,  $witamin_b12,  $witamin_c,  $witamin_d,  $witamin_e, $witamin_pp, $calcium, $chlorine, $magnesium, $phosphorus, $potassium, $sodium, $iron, $zinc, $copper,  $manganese, $molybdenum, $iodine, $fluorine, $chrome, $selenium, $description, $userid);
+     $updatedProduct = new ProductController($properties['Name'],  $properties['kCal'],  $properties['Protein'],  $properties['Ani_ Protein'], $properties['Vege_ Protein'],  $properties['Fat'],  $properties['Sat_ Fat'],  $properties['Mono_Fat'], $properties['Poly_Fat'], $properties['Omega3'], $properties['Omega6'], $properties['Carbs'], $properties['Netto_Carbs'], $properties['Sugar'], $properties['Fiber'],  $properties['Salt'], $properties['Cholesterol'], $properties['Witamin_K'], $properties['Witamin_A'], $properties['Witamin_B1'], $properties['Witamin_B2'],  $properties['Witamin_B5'], $properties['Witamin_B6'],  $properties['Biotin'],  $properties['Folic_Acid'],  $properties['Witamin_B12'],  $properties['Witamin_C'],  $properties['Witamin_D'],  $properties['Witamin_E'], $properties['Witamin_PP'], $properties['Calcium'], $properties['Chlorine'], $properties['Magnesium'], $properties['Phosphorus'], $properties['Potassium'], $properties['Sodium'], $properties['Iron'], $properties['Zinc'], $properties['Copper'],  $properties['Manganese'], $properties['Molybdenum'], $properties['Iodine'], $properties['Fluorine'], $properties['Chrome'], $properties['Selenium'], $properties['Description'], $properties['userid']);
+
 
     if(!$file->set_file($photo)){
 
@@ -129,6 +79,6 @@ if(isset($_POST['submit'])){
         header("location: ../add_product.php?error=save");
     }
 
-}
+
 
 ?>
