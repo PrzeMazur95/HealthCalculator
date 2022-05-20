62f605cbe09256abfa86e5bf07a622cae683f18a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-20 19:05:47 +0200

    Change error content

--- products.php
+++ products.php
@@ -87,7 +87,7 @@
                                         switch ($_GET['error']){
 
                                             case "id":
-                                                echo "You could not delete or edit this product, it is not yours!";
+                                                echo "You could not delete or edit this product, it is not yours, or you have typed wrong informations!";
                                                 break;
 
                                             case "stmtfailed":
