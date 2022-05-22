8a38ace9e0c61c08d9c895a10b0bc48f7b7cf323
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-22 20:38:18 +0200

    Ad proper rediretion to products on index page

--- index.php
+++ index.php
@@ -136,9 +136,9 @@
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-body">Products</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="products.php">View all products!</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
