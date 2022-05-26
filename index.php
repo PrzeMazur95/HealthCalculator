aa0ff9939f4ba45fb21f98a129eb4f64e73b31f5
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-26 22:42:08 +0200

    Add shortcut to a calculator page

--- index.php
+++ index.php
@@ -145,9 +145,9 @@
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                                    <div class="card-body">Calculator</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="calculator.php">Calculate your meals!</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
