d106920c8733ab56257bf85553f86b5c926961a8
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-06-01 21:46:32 +0200

    Add sql to calulator model class

--- classes/Calculator.class.php
+++ classes/Calculator.class.php
@@ -4,9 +4,10 @@ class Calculator extends Dbh {
 
     protected function setMeal($userId, $productId, $quantity, $date){
 
-
+        $stmt = $this->connect()->prepare('INSERT INTO Calculator (user_id, product_id, quantity, datte) VALUES (?,?,?,?)');
 
     }
     
 }
 ?>
+
