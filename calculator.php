cc23a54b271b6f5a44e94f2c13d29388db972e0d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-06-06 16:24:26 +0200

    Add sample table to show more informations

--- calculator.php
+++ calculator.php
@@ -196,8 +196,29 @@
                                         </tr>
                                     </tbody>
                                 </table>
-                                <button class="btn btn-primary text-center">End this day, and start a new one!</button>
+                                <button class="btn btn-info text-center">Get more informations!</button>
                                 </div>
+                                    <table class='table table-condensed table-striped'>
+                                        <tr>
+                                            <td>one</td>
+                                            <td>two</td>
+                                            <td>three</td>
+                                        </tr>
+                                        <tr>
+                                            <td>one</td>
+                                            <td>
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+                                            </td>
+                                            <td>three</td>
+                                        </tr>
+                                        <tr>
+                                            <td>one</td>
+                                            <td>two</td>
+                                            <td>three</td>
+                                        </tr>
+                                    </table>
                             </div> 
                         </div>
                 </main>
